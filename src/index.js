419c23e3d6e3a7bac1a14679aa568e74c1a5ce61
karticksriram <sriram5130@gmail.com>
2021-12-05 00:47:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Login from './Login';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import './bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
