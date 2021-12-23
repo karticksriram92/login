9d5f22411b4e41a201c474bbfd8abd02c484ea2b
karticksriram <sriram5130@gmail.com>
2021-12-23 08:59:30 +0000

    test validation

--- src/Login.jsx
+++ src/Login.jsx
@@ -14,6 +14,7 @@ class Login extends Component {
     
     handleSubmit = e => {
         e.preventDefault();
+		console.log("enter")
 		
 		const errors["username"] = "username is needed."
 		this.setState({ errors })
