78f92d9232282195d2b9d543f2f30557a714a0c4
karticksriram92 <sriram5130@gmail.com>
2022-01-13 02:07:34 +0530

    validate with alert1

--- src/Input.jsx
+++ src/Input.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 
 const Input = ({ name, value, error, onChange }) => {
 	return (
+		<div>
 		<input autoFocus 
 			value={value} 
 			onChange={onChange} 
 			name={name}
 			id={name} 
 			className="form-control" 
-			type="text" 
+			type="text"
+			error={error}
 		/>
 		{ error && <div className="alert alert-danger">{ error }</div> }
+		</div>
 )};
 
 export default Input;
